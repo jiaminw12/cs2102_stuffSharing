6cdd5d9bf25e4b013697174bb3d5bdbdc075c469
jiaminw12 <jiaminw@ymail.com>
2016-04-02 13:37:38 +0800

    For search.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 $current_page = 'Home';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header("Location: search.php?keyword=". $_POST['searchText']);
+}
 ?>
 
 <?php ob_start(); ?>
@@ -8,19 +12,24 @@ $current_page = 'Home';
 <div class="inner cover">
     <h1 class="cover-heading">Welcome to ShareIt</h1>
     <p class="lead">Stuff Sharing site</p>
-    <p class="lead">
-        <div id="imaginary_container"> 
-            <div class="input-group stylish-input-group">
-                <input id="searchText" type="text" class="form-control" placeholder="Search" >
-                <span class="input-group-addon">
-                    <button type="submit">
-                        <span class="glyphicon glyphicon-search"></span>
-                    </button>  
-                </span>
+    <p></p>
+    <p>testing 2</p>
+    <form method="post" role="form">
+        <div class="form-group">
+            <div id="imaginary_container"> 
+                <div class="input-group stylish-input-group">
+                    <input id="searchText" name="searchText" type="text" class="form-control" placeholder="Search...">
+                    <span class="input-group-addon">
+                        <button id="submit" name="submit" type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>    
+                    </span>
+                </div>
             </div>
         </div>
-</p>
+    </form>
 </div>
+
 <?php
 $content = ob_get_clean();
 include_once 'template/skeleton.php';
