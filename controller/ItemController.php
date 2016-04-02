faece3ac9a2af6dd3cbc70a33bc4384e8b9b54e2
jiaminw12 <jiaminw@ymail.com>
2016-04-02 23:44:13 +0800

    Updated itemController

--- controller/ItemController.php
+++ controller/ItemController.php
@@ -31,7 +31,7 @@ namespace ItemController {
         $result = \DBHandler::execute($statement, true);
 
         if (count($result) != 1) {
-            return null;
+            return NULL;
         } else {
             $result = $result[0];
             $items[] = new \Item($result[0], $result[1], $result[2], $result[3], $result[4], $result[5], $result[6], $result[7], $result[8], $result[9], $result[10], $result[11], $result[12]);
